b2b616e80283b5624cfe8e93e394abcd3c90439d
Arnab Sardar <44030498+Arnab0999@users.noreply.github.com>
2019-03-18 21:59:50 +0530

    Update Q2.c

--- Assignment3/Q2.c
+++ Assignment3/Q2.c
@@ -4,12 +4,39 @@ float f(float x)
 {
          return ( ( 2000 * log ( 140000/(140000-2100*x))) - (9.8*x));
 }
+float SimpsonsRule(float a,float b,int n)
+{
+    float h=(b-a)/n;
+    float result=0;
+    for(int i = 0; i <= n; i++)
+    {
+        if(i==0||i==n)
+        {
+            if(i==0)
+                result+=f(a);
+            else
+                result+=f(b);
+            
+        }
+        else if(i%2==0)
+        {
+            result+=2*f(a+i*h);
+        }
+        else
+        {
+            result+=4*f(a+i*h);
+        }
+    }
+    return result*(h/3);
+}
 int main()
 {
         float a,b;
-        printf("Enter the values of a and b:");
+        int n;
+        printf("Enter the values of lower and upper limits:");
         scanf("%f%f",&a,&b);
-        float integration=((b-a)/6)*(f(a)+4*f((a+b)/2)+f(b));
-        printf("\nIntegrated value is :%f",integration);
+        printf("Enter number of divisions:");
+        scanf("%d",&n);
+        printf("Result=%5f\n",SimpsonsRule(a,b,n));
         return 0;
 }
