5b66e014a2629cc3e8376fca117f137177c5d81c
Arnab Sardar <44030498+Arnab0999@users.noreply.github.com>
2019-03-18 20:51:58 +0530

    Update Q1.c

--- Assignment3/Q1.c
+++ Assignment3/Q1.c
@@ -2,14 +2,26 @@
 #include <math.h>
 
 float f(float x){
+
         return ( ( 2000 * log ( 140000/(140000-2100*x))) - (9.8*x));
 }
-
+float trapezoidalRule(float a,float b,int n)
+{
+    float h=(b-a)/n;
+    float result=f(a)+f(b);
+    for(int i = 0; i < n; i++)
+    {
+        result+=f(a+i*h);
+    }
+    return (h/2)*result;
+}
 int main(){
         float a,b;
-        printf("Enter the values of a and b:");
+        int n;
+        printf("Enter the lower and upper limits:");
         scanf("%f%f",&a,&b);
-        float intg = (b-a)*( (f(a)+f(b)) / 2 );
-        printf("%f\n",intg);
+        printf("Enter no. of division:");
+	    scanf("%d",&n);
+	    printf("Result=%.5f\n",trapezoidalRule(a,b,n));
         return 0;
 }
